35a8c42dbb9cbdf55110e9682d9ebb1bb0085721
Daniel Hoang <107479212+danhoangg@users.noreply.github.com>
2023-12-13 21:54:30 +0000

    Create p2.py

---
+++ day9/p2.py
@@ -0,0 +1,38 @@
+sequences = []
+
+with open('input.txt') as f:
+    lines = f.read().split("\n")
+
+    for line in lines:
+        sequences.append(list(map(lambda x: int(x), line.split())))
+
+
+def check(s):
+    if not s:
+        return True
+
+    allzeros = True
+    for i in s:
+        if i != 0:
+            allzeros = False
+            break
+    return allzeros
+
+
+first = []
+res = 0
+
+for s in sequences:
+    dp = s
+    while not check(dp):
+        first.append(dp[0])
+        for i in range(len(dp)-1):
+            dp[i] = dp[i+1] - dp[i]
+        del dp[-1]
+    for i in range(len(first)-1, 0, -1):
+        first[i-1] -= first[i]
+    res += first[0]
+    first = []
+
+print(res)
+
